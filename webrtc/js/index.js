f13d2e8ee70f0608e116dbf7be27e822eb3f844e
Ryan Sukale <ryansukale@gmail.com>
2014-02-09 13:59:53 -0500

    webrtc:basic recording

---
+++ webrtc/js/index.js
@@ -0,0 +1,24 @@
+$(function(){
+
+	$('.btn-start-rec').on('click',function(event){
+		var $this = $(this);
+		var $target = $(event.target);
+		
+		navigator.getUserMedia({audio: true}, function(mediaStream) {
+			 window.recordRTC = RecordRTC(mediaStream);
+			 recordRTC.startRecording();
+		});
+
+	});
+	
+	$('.btn-stop-rec').on('click',function(event){
+		var $this = $(this);
+		var $target = $(event.target);
+		
+		recordRTC.stopRecording(function(audioURL) {
+			window.open(audioURL);
+		});
+		
+	});
+	
+});
